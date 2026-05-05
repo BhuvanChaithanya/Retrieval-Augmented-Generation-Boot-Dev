a7ff14a0b05caaf0940ebb11c7d7bf78262b001d
Reddyvari Bhuvan Chaithanya Reddy <99655691+BhuvanChaithanya@users.noreply.github.com>
2026-05-05 17:21:03 +0000

    implemented keyword search

--- rag-search-engine/cli/keyword_search_cli.py
+++ rag-search-engine/cli/keyword_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-
+from lib.keyword_search import search_command
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -13,6 +13,11 @@ def main() -> None:
         case "search":
             # print the search query here
             print(f"Searching for: {args.query}")
+            results = search_command(args.query, 5)
+            for i, result in enumerate(results):
+                print(f"{i+1} {result}")
+
+
         case _:
             parser.print_help()
 
