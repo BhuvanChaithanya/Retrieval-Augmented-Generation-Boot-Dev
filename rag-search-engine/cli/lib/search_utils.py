8160d81f98b6cf08dae62fedee6e7e20edd19ee7
Reddyvari Bhuvan Chaithanya Reddy <99655691+BhuvanChaithanya@users.noreply.github.com>
2026-05-07 16:59:11 +0000

    removed the stop words from the tokens

--- rag-search-engine/cli/lib/search_utils.py
+++ rag-search-engine/cli/lib/search_utils.py
@@ -2,9 +2,15 @@ import json
 from pathlib import Path 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_PATH = PROJECT_ROOT/'data/movies.json'
-
+DATA_PATH = PROJECT_ROOT/'data'
+MOVIE_PATH = DATA_PATH/'movies.json'
+STOPWORD_PATH = DATA_PATH/'stopwords.txt'
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, "r") as f:
+    with open(MOVIE_PATH, "r") as f:
         data = json.load(f)
     return data['movies']
+
+def load_stopwords() -> list[str]:
+    with open(STOPWORD_PATH, "r") as f:
+        data = f.read().splitlines()
+    return data
